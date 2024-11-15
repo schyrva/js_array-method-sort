d8677d49223eaea5a674c464765d5b56fb55f313
Stanislav Chyrva <alkefirko@gmail.com>
2024-11-15 19:51:04 +0200

    default function moved outside the method

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -1,30 +1,28 @@
 'use strict';
 
+function defaultCompareFunction(a, b) {
+  const strA = String(a);
+  const strB = String(b);
+
+  if (strA < strB) {
+    return -1;
+  }
+
+  if (strA > strB) {
+    return 1;
+  }
+
+  return 0;
+}
+
 /**
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function (compareFunction) {
-    const compareFn =
-      compareFunction ||
-      function (a, b) {
-        const strA = String(a);
-        const strB = String(b);
-
-        if (strA < strB) {
-          return -1;
-        }
-
-        if (strA > strB) {
-          return 1;
-        }
-
-        return 0;
-      };
-
+  [].__proto__.sort2 = function (compareFunction = defaultCompareFunction) {
     for (let i = 0; i < this.length; i++) {
       for (let j = 0; j < this.length - i - 1; j++) {
-        if (compareFn(this[j], this[j + 1]) > 0) {
+        if (compareFunction(this[j], this[j + 1]) > 0) {
           const temp = this[j];
 
           this[j] = this[j + 1];
